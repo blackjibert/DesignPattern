edcccf6d5aebaf1d93dd78fa292e7e4b9c45b6de
Joshua <1257699625@qq.com>
2025-02-16 15:46:22 +0800

    单例模式

---
+++ DesignPattern/Pattern01Singleton/Counter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern01Singleton
+{
+    internal  sealed class Counter
+    {
+        public Counter()
+        {
+            CreateTime=DateTime.Now;
+            Console.WriteLine($"实例化对象:{CreateTime}");
+        }
+        public DateTime CreateTime { get; private set; }
+
+        #region 第1种,懒汉式单例模式,适用于单线程环境,getInstance() 方法被调用时才创建
+        private static Counter singleton = null;
+        public static Counter getInstance()
+        {
+            if (singleton == null)
+            {
+                singleton = new Counter();
+            }
+            return singleton;
+        }
+        #endregion
+        #region 第2种,饿汉式单例模式, 在类加载时就被自动初始化,并且在程序运行期间保持唯一实例,避免多线程问题。
+        //public static Counter Instance=new Counter();
+        #endregion
+
+        #region 第3种,改进的懒汉式单例模式,加入双重检查锁
+        private static readonly object lockObj = new object();
+        public static Counter getInstanceLock()
+        {
+            if (singleton == null)  // 第一次检查（提高性能）
+            {
+                lock (lockObj)  // 线程同步
+                {
+                    if (singleton == null)  // 第二次检查（确保单例）
+                    {
+                        singleton = new Counter();
+                    }
+                }
+            }
+            return singleton;
+        }
+        #endregion
+        #region 第4种,懒加载lazy
+        private static readonly Lazy<Counter> lazyInstance = new Lazy<Counter>(() => new Counter());
+        public static Counter LazyInstance => lazyInstance.Value;
+        #endregion
+
+        //第5种,IOC容器单例注册,prism
+    }
+}
---
+++ DesignPattern/Pattern01Singleton/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Pattern01Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //Counter counter5 = Counter.getInstanceLock();
+                //Counter counter6 = Counter.getInstanceLock();
+
+                // 开启一个新的任务（线程）,实现多线程
+                Task.Factory.StartNew(()=>
+                {
+                    //饿汉式单例模式调用
+                    //Counter counter1 = Counter.getInstance();
+                    //Counter counter2 = Counter.getInstance();
+
+                    //饿汉式单例模式调用
+                    //Counter counter3 = Counter.Instance;
+                    //Counter counter4= Counter.Instance;
+
+                    //加入双重检查锁的单例模式调用,只会调用一次
+                    //Counter counter5 = Counter.getInstanceLock();
+                    //Counter counter6 = Counter.getInstanceLock();
+
+                    //lazy懒加载
+                    Counter counter7 = Counter.LazyInstance;
+                    Counter counter8 = Counter.LazyInstance;
+                });
+            }
+            //暂停一下
+            Console.ReadKey();
+        }
+    }
+}
